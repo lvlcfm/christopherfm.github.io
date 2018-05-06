dd21945b25ffb001e2ac6342599b2225dd71fae7
Franco <christopher.franco.m@gmail.com>
2018-05-06 02:53:41 -0700

    name logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,20 @@
 import React from "react"
 import '../styles/index.scss'
+import Francotorium from '../images/francotoriumsvg_animated.svg'
 
 export default class IndexPage extends React.Component {
 render() {
         return (
             <div className="container">
-                <h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
 
-<h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
-<h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
-<h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
 
-<h1 className="">FRANCO</h1>
                 <h1 className="">FRANCO</h1>
+<img className="large" src={Francotorium} />
+
                 <h1 className="">FRANCO</h1>
-<h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
-<h1 className="">FRANCO</h1>
-<h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
-                <h1 className="">FRANCO</h1>
+<img src={Francotorium} />
+
+
             </div>
         );
     }
