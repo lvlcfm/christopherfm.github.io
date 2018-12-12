88feacac3b940dcd09d26f111fcfc1853ef1298b
Christopher Franco <himahmago@gmail.com>
2018-12-12 01:14:01 -0800

    added gatsby google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-sass`]
+  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-sass`,{
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-130870802-1",
+      // Puts tracking script in the head instead of the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+      // Setting this parameter is also optional
+      respectDNT: true,
+      // Avoids sending pageview hits from custom paths
+      exclude: ["/preview/**", "/do-not-track/me/too/"],
+      // Enables Google Optimize using your container Id
+      optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+      // Any additional create only fields (optional)
+      sampleRate: 5,
+      siteSpeedSampleRate: 10,
+    },
+  },]
 };
