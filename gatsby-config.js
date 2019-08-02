eea799fef165ab695d1fcb771ab095522330b033
Christopher Franco Monterrosa <himahmago@gmail.com>
2019-08-02 02:36:52 -0700

    design: NEW LOGO AYY 🔥

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/mahMago-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/francogments-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { css } from '@emotion/core'
-import Francogments from '../assets/mahMago.svg'
+import Francogments from '../assets/francogments.svg'
 
 const IndexPage = () => (
   <Layout>
