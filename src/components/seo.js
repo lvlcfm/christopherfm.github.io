0b85b1147185bd03100e76475202236eaf578d80
Christopher Franco Monterrosa <himahmago@gmail.com>
2019-08-06 23:31:47 -0700

    seo: twitter card part 2

--- src/components/seo.js
+++ src/components/seo.js
@@ -53,7 +53,7 @@ function SEO({ description, lang, meta, keywords, title }) {
               },
               {
                 name: 'twitter:image',
-                content: Francogments,
+                content: { Francogments },
               },
             ]
               .concat(
