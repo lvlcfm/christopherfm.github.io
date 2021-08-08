ebabf6aadafc97b39757c70bded1c8c07eb37ce2
Christopher Franco Monterrosa <lvlcfm@christopher.fm>
2021-08-07 21:42:02 -0700

    feat(about): adds styling for about page with small bio blurb

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,20 +9,39 @@ export default function Projects() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          Hello, I&apos;m Christopher Franco Monterrosa — I&apos;m a software
-          engineer.
-        </p>
-      </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>About</h2>
         <Image
           src="/images/profile.svg"
           alt="Man with box in over his head"
-          width={1442}
-          height={964}
+          width={816.73}
+          height={546}
         />
+        <div className={utilStyles.aboutSlogan}>
+          he thinks both inside and outside the box
+        </div>
+      </section>
+      <section>
+        <div className={utilStyles.CFM}>Christopher Franco Monterrosa</div>
+        <div className={utilStyles.bioBoxContainer}>
+          <div className={utilStyles.bioBox}>
+            <p>
+              Christopher Franco Monterrosa is a Full Stack Engineer and
+              creative technologist. He enjoys building solutions with people
+              and communities — whether it be tools, products or systems.
+            </p>
+            <p>He loves engineering with purpose.</p>
+            <p>
+              His journey as a Software Engineer has had him running a fashion
+              tech startup, to working at medium and big sized companies such as
+              Glassdoor and vmware to leading engineering efforts at YC backed
+              companies.
+            </p>
+            <p>
+              Ask him about Smart Cities, Open Source, Music, Design,Cycling,
+              Productivity,Movies, Writing, Books, Dancing, Anime and gaming.
+            </p>
+          </div>
+        </div>
       </section>
     </Layout>
   );
