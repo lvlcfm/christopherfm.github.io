902b938f4b16197dc06bbe76911228f112127b6b
Christopher Franco Monterrosa <lvlcfm@christopher.fm>
2021-08-10 10:24:38 -0700

    feature(analytics): adds fathom as analytics of choice

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import '../styles/globals.css'
-import { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
+import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    if (process.env.NODE_ENV === 'production') {
+      Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID, {
+        includedDomains: ['christopher.fm', 'www.christopher.fm'],
+      });
+    }
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+
+  return <Component {...pageProps} />;
 }
