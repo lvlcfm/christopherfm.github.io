224006848803d786b48d9ee2c50ab81c474ebc48
Christopher Franco Monterrosa <lvlcfm@christopher.fm>
2021-08-21 22:28:41 -0700

    prose(screenplay - pathrise): adds screenplay for pathrise work experience

--- pages/work/pathrise.tsx
+++ pages/work/pathrise.tsx
@@ -1,6 +1,7 @@
 import Layout from '../../components/layout';
 import Image from 'next/image';
 import utilStyles from '../../styles/utils.module.css';
+import utilScriptStyles from '../../styles/utilsScreenPlay.module.css';
 
 export default function Pathrise() {
   return (
@@ -12,6 +13,366 @@ export default function Pathrise() {
           width={700}
           height={602}
         />
+      </section>
+      <section>
+        <div className={utilScriptStyles.scriptIntroContainer}>
+          <div className={utilScriptStyles.scriptTitleContainer}>
+            <div>PATHRISE</div>
+            <div>SERIES A ENGINEER</div>
+          </div>
+          <div className={utilScriptStyles.scriptCredit}>Written by</div>
+          <div className={utilScriptStyles.scriptAuthor}>
+            Christopher Franco Monterrosa
+          </div>
+          <div className={utilScriptStyles.scriptDate}>21 August 2021</div>
+          <p>
+            It&apos;s the Fall of 2019, Christopher Franco Monterrosa, has just
+            finished leading engineering initiatives at an early stage Y
+            Combinator backed startup as a Full Stack Engineer.
+          </p>
+          <p>
+            But now he wants to know what it takes to build a company that
+            obtains a Series A.
+          </p>
+          <p>
+            He joins Pathrise, a Y Combinator (18) backed startup. They are an
+            online mentorship program that helps job seekers in tech find the
+            best job possible through personalized and data-driven support.
+          </p>
+          <p>
+            He gets hired as a software engineer intern, eventually taking up
+            more responsibility to be the sole Software Engineer on the team
+            towards the end of his tenure.
+          </p>
+          <p>
+            He&apos;s part of an engineering team that mentors him, providing
+            opportunities to grown and learn. He works with a Product Manager
+            who is a PM Paragon — a golden gate bridge of product, design and
+            engineering.
+          </p>
+          <p>
+            While Christopher Franco Monterrosa gets what he wants, he also
+            unexpectedly gets what he needs — becoming a Series A Engineer.
+          </p>
+          <p>
+            In the words of his friend — he brings passion, innate leadership
+            abilities, with an understanding of the greater business context and
+            an eternal pursuit for great UX.
+          </p>
+          <p>
+            Rearchitectures, refactors, devops, features, bugs and all — with
+            ownership and a deep sense of curiosity — it&apos;s a memorable
+            first full-time software engineering experience for Christopher
+            Franco Monterrosa.
+          </p>
+          <p>
+            This story takes place before the company gets their Series A - it
+            focuses on Christopher Franco Monterrosa&apos;s journey on becoming
+            a Series A Engineer.
+          </p>
+          <p>We begin in Downtown San Francisco.</p>
+          <div className={utilScriptStyles.scriptSceneHeading}>
+            INT. SAN FRANCISCO / MONTGOMERY / PATHRISE HQ - EVENING
+          </div>
+          <p>
+            CHRISTOPHER FRANCO MONTERROSA, is wearing his black vmware OSPO
+            (open source program office) sweater, with a denim button up
+            underneath. His ocean of curls have become a frozen whirlpool bun -
+            revealing his turquoise-brown rimmed glasses.
+          </p>
+          <p>He&apos;s sitting at a desk, we see his bright red backpack.</p>
+          <div className={utilScriptStyles.scriptMetaDescription}>
+            WE HEAR A SLACK NOTIFICATION
+          </div>
+          <p>
+            The lead engineer has left comments on his Pull Request. All her
+            comments are on point, he needs to address those to earn his LGTM
+            (looks good to me).
+          </p>
+          <p>
+            He glances at his monitor, it has a technical design doc he wrote -
+            it shows architecture diagrams, tradeoffs to different approaches.
+          </p>
+          <p>He switches over to his text-editor.</p>
+          <p>
+            ANGLE ON KEYBOARD - we hear rapid keystrokes, each with a purpose -
+            no backspace.
+          </p>
+          <p>He&apos;s addressed all the comments on the PR (Pull Request).</p>
+          <p>He commits and pushes his code</p>
+          <div className={utilScriptStyles.scriptMetaDescription}>
+            WE HEAR A SLACK NOTIFICATION
+          </div>
+          <p>LGTM has arrived. It&apos;s time to ship.</p>
+          <div className={utilScriptStyles.scriptDialogueContainer}>
+            <div className={utilScriptStyles.scriptDialogue}>
+              <div className={utilScriptStyles.scriptActor}>FRANCO (V.O)</div>
+              <div>
+                Hey there — have you ever had to completely change the way
+                people access your platform? As of a week ago, I didn&apos;t
+                know how I would do it, but with an ounce of curiosity, I got a
+                pound of code ready to ship.
+              </div>
+            </div>
+          </div>
+          <p>
+            With over a month of PRs (Pull Requests) under his belt, he deploys
+            his first big feature.
+          </p>
+          <div className={utilScriptStyles.scriptSceneHeading}>
+            INT. DALY CITY / FRANCO’S HOME / LIVING ROOM - EVENING
+          </div>
+          <p>
+            Franco is sitting on his couch cross-legged, donning his navy blue
+            joggers and vmware OSPO (open source program office) black sweater,
+            with a light blue denim button up - its become his uniform.
+          </p>
+          <p>
+            His setup is a makeshift workstation, with a monitor stacked up on
+            top of two thick textbooks
+          </p>
+          <div className={utilScriptStyles.scriptMetaDescription}>
+            THEY READ
+          </div>
+          <p>DRAWING IDEAS A HAND-DRAWN APPROACH FOR BETTER DESIGN</p>
+          <p>OPERATING SYSTEMS PRINCIPLES & PRACTICE 2ND EDITION</p>
+          <p>
+            Directly below the monitor his hands are placed on his laptop with
+            the terminal opened. He&apos;s accompanied by a cup containing
+            enough coffee to remind you of the taste but not enough for a jolt.
+          </p>
+          <div className={utilScriptStyles.scriptDialogueContainer}>
+            <div className={utilScriptStyles.scriptDialogue}>
+              <div className={utilScriptStyles.scriptActor}>FRANCO (V.O)</div>
+              <div>
+                Hello again, have you ever launched something on Product Hunt?
+                If not, don&apos;t worry, neither have I! But what I&apos;m
+                about to push will be making it&apos;s debut on my behalf
+              </div>
+            </div>
+          </div>
+          <p>He deploys another feature</p>
+          <div className={utilScriptStyles.scriptSceneHeading}>
+            INT. DALY CITY / FRANCO&apos;S HOME / FRANCO&apos;S ROOM - MORNING
+          </div>
+          <p>
+            We see Franco&apos;s face — he&apos;s smiling, his keyboard cadence
+            a little faster than usual.
+          </p>
+          <p>
+            We zoom out, it’s a full body mirror spanning across the whole room.
+          </p>
+          <p>Cyber Titan Atlas appears again.</p>
+          <p>
+            Franco is sitting in a green wooden chair. We see a used surgical
+            mask next to his 1951 coffee mug.
+          </p>
+          <div className={utilScriptStyles.scriptDialogueContainer}>
+            <div className={utilScriptStyles.scriptDialogue}>
+              <div className={utilScriptStyles.scriptActor}>FRANCO (V.O)</div>
+              <div>
+                You know, the best type of performance optimizations, are the
+                ones that support good design. This refactor shaves off a few
+                seconds, and with a loading indicator to provide some system
+                feedback, the waiting will feel more like progress and less like
+                traffic on the bay bridge. UX driven engineering.
+              </div>
+            </div>
+          </div>
+          <p>He pushes another feature</p>
+          <div className={utilScriptStyles.scriptSceneHeading}>
+            INT. DALY CITY / FRANCO&apos;S HOME / FRANCO&apos;S ROOM - DAY
+          </div>
+          <p>Franco is preparing for a big feature.</p>
+          <div className={utilScriptStyles.scriptDialogueContainer}>
+            <div className={utilScriptStyles.scriptDialogue}>
+              <div className={utilScriptStyles.scriptActor}>FRANCO (V.O)</div>
+              <div>
+                These are a few of my favorite things. First, this part — as an
+                engineer, you can build anything, but to build something you
+                begin adding the contraints one by one — eventually you&apos;re
+                sculpting a solution on the fly during the meeting.
+              </div>
+            </div>
+          </div>
+          <p>
+            We see Franco having a meeting through video chat with his Product
+            Manager to go over deliverables, design and timelines
+          </p>
+          <div className={utilScriptStyles.scriptDialogueContainer}>
+            <div className={utilScriptStyles.scriptDialogue}>
+              <div className={utilScriptStyles.scriptActor}>FRANCO (V.O)</div>
+              <div>
+                Then, another favorite — the technical design doc. Not
+                mandatory, but who doesn&apos;t love an origin story? A north
+                star that grounds, shapes and helps facilitate present and
+                future technical discussions.
+              </div>
+            </div>
+          </div>
+          <p>
+            He creates a technical design doc based on the meeting and begins
+            typing.
+          </p>
+          <div className={utilScriptStyles.scriptDialogueContainer}>
+            <div className={utilScriptStyles.scriptDialogue}>
+              <div className={utilScriptStyles.scriptActor}>FRANCO (V.O)</div>
+              <div>
+                This is also my favorite part, especially for big features,
+                it&apos;s providing a visual guide to understanding
+                architectural changes.
+              </div>
+            </div>
+          </div>
+          <p>
+            He opens up his favorite design tool that starts with an F and
+            begins drawing architectural diagrams
+          </p>
+          <div className={utilScriptStyles.scriptCameraDirection}>
+            JUMP CUT:
+          </div>
+          <p>
+            We see another meeting over video - he&apos;s discussing with the
+            Lead Engineer and CTO to get feedback on the proposed changes. It
+            ends.
+          </p>
+          <div className={utilScriptStyles.scriptDialogueContainer}>
+            <div className={utilScriptStyles.scriptDialogue}>
+              <div className={utilScriptStyles.scriptActor}>FRANCO (V.O)</div>
+              <div>
+                I think that meeting went well! Just need to make a few
+                adjustments...
+              </div>
+            </div>
+          </div>
+          <p>
+            He opens the visual guide, the technical design doc, vector and
+            prose. The edits eventually lead to code.
+          </p>
+          <div className={utilScriptStyles.scriptSceneHeading}>
+            INT. DALY CITY / FRANCO’S HOME / THE GARAGE - EVENING
+          </div>
+          <p>The deadline to ship this new experience is closing in.</p>
+          <div className={utilScriptStyles.scriptCameraDirection}>
+            QUICK SHOTS:
+          </div>
+          <p>
+            franco types &quot;commit -m&quot; in his terminal with a message
+          </p>
+          <p>he types &quot;git push&quot;</p>
+          <p>clicks on &quot;Rebase and merge&quot;</p>
+          <p>we see him join a video call for the daily team sync </p>
+          <p>he clicks the hang up button, ends call</p>
+          <p>he types git checkout, git pull, git branch -b franco/</p>
+          <p>grabs his 1951 coffee mug, sips it, sets it down</p>
+          <div className={utilScriptStyles.scriptCameraDirection}>
+            QUICK SHOTS:
+          </div>
+          <p>
+            franco types &quot;commit -m&quot; in his terminal with a message
+          </p>
+          <p>he types &quot;git push&quot;</p>
+          <p>clicks on &quot;Rebase and merge&quot;</p>
+          <p>we see him join a video call for the daily team sync </p>
+          <p>he clicks the hang up button, ends call</p>
+          <p>he types git checkout, git pull, git branch -b franco/</p>
+          <p>grabs his 1951 coffee mug, sips it, sets it down</p>
+          <div className={utilScriptStyles.scriptCameraDirection}>
+            QUICK SHOTS:
+          </div>
+          <p>
+            franco types &quot;commit -m&quot; in his terminal with a message
+          </p>
+          <p>he types &quot;git push&quot;</p>
+          <p>clicks on &quot;Rebase and merge&quot;</p>
+          <p>we see him join a video call for the daily team sync </p>
+          <p>he clicks the hang up button, ends call</p>
+          <p>he types git checkout, git pull, git branch -b franco/</p>
+          <div className={utilScriptStyles.scriptCameraDirection}>
+            JUMP CUT:
+          </div>
+          <p>ANGLE ON FRANCO - he grabs his 1951 coffee mug</p>
+          <div className={utilScriptStyles.scriptCameraDirection}>
+            JUMP CUT:
+          </div>
+          <div className={utilScriptStyles.scriptSceneHeading}>
+            INT. DALY CITY / FRANCO’S HOME / THE GARAGE - EVENING
+          </div>
+          <p>
+            Franco takes a sip of coffee from his 1951 Coffee Mug and sets it
+            down.
+          </p>
+          <p>We take a look at the clock.</p>
+          <p>
+            We hear the second hand echo throughout the room like a heartbeat.
+          </p>
+          <p>He&apos;s stopped typing.</p>
+          <p>
+            The feature, the new experience that he&apos;s been working on with
+            the team is finally complete.
+          </p>
+          <p>Franco sighs with deep satisfaction after a marathon of work.</p>
+          <div className={utilScriptStyles.scriptDialogueContainer}>
+            <div className={utilScriptStyles.scriptDialogue}>
+              <div className={utilScriptStyles.scriptActor}>FRANCO (V.O)</div>
+              <div>it&apos;s done</div>
+            </div>
+          </div>
+          <p>
+            He pulls up the prototype one last time. The techinical design doc.
+            The requirements, the list of bugs resolved along the way.
+          </p>
+          <p>
+            He stares at the screen -- eleven thousand new lines of code. The
+            vision that was only known through prototype was hours away from
+            being experienced by mentors, fellows and all.
+          </p>
+          <div className={utilScriptStyles.scriptDialogueContainer}>
+            <div className={utilScriptStyles.scriptDialogue}>
+              <div className={utilScriptStyles.scriptActor}>FRANCO</div>
+              <div>ALRIGHT, IT IS TIME!</div>
+            </div>
+          </div>
+          <p>
+            Franco goes to the release branch and presses the button to kick off
+            the launch — &quot;Rebase and merge&quot;
+          </p>
+          <p>He pushes another feature</p>
+          <div className={utilScriptStyles.scriptSceneHeading}>
+            INT. DALY CITY / FRANCO’S HOME / LAUNCH DAY - MORNING
+          </div>
+          <div className={utilScriptStyles.scriptMetaDescription}>
+            WE HEAR A SLACK NOTIFICATION
+          </div>
+          <p>
+            It&apos;s the morning of the launch, we see Franco walking towards
+            his Cyber Titan Atlas — he walks slower and then slower as he
+            notices the red number next to the Pathrise slack workspace icon.
+          </p>
+          <p>
+            He musters up the courage and clicks on one of the channels and
+            starts reading the first message he sees
+          </p>
+          <div className={utilScriptStyles.scriptMetaDescription}>
+            &quot;I&apos;m freaking out this is the coolest thing of all
+            time&quot;
+          </div>
+          <p>
+            Franco is smiling, the type of smile that whispers &quot;thank
+            you&quot;.
+          </p>
+          <p>
+            His eyes show the deepest sense of gratitude to the work he did with
+            the team.
+          </p>
+          <p>He whispers to himself</p>
+          <div className={utilScriptStyles.scriptDialogueContainer}>
+            <div className={utilScriptStyles.scriptDialogue}>
+              <div className={utilScriptStyles.scriptActor}>FRANCO</div>
+              <div> Series A, here we come.</div>
+            </div>
+          </div>
+        </div>
       </section>
     </Layout>
   );
